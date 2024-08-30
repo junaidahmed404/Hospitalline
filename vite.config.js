866348b33c08d7a55f0b98c6daec432eb1d95539
Junaid Ahmed <bhattijunaidahmed404@gmail.com>
2024-08-30 18:04:39 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Hospitalline/', // Make sure this matches your GitHub repository name
   plugins: [react()],
-})
+});
