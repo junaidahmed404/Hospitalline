77beb5b47d15489b83c947ca331f63d4783205b5
Junaid Ahmed <bhattijunaidahmed404@gmail.com>
2024-09-03 08:15:47 +0500

    Update DemoVersion.jsx

--- src/components/DemoVersion.jsx
+++ src/components/DemoVersion.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./style/demoSection.css";
 import ImageThree from "../assets/ImageFive.png";
 const demoSection = () => {
@@ -37,43 +36,3 @@ const demoSection = () => {
 };
 
 export default demoSection;
-=======
-import "./style/demoSection.css";
-import ImageThree from "../assets/ImageFive.png";
-const demoSection = () => {
-  return (
-    <section id="demo" className="demo-section section">
-      <div className="demo-container">
-        <div className="text">
-          <h2 className="demo-heading">Explore Our Demo Version</h2>
-          <p className="demo-paragraph">
-            We invite you to experience the Hospitalline Application through our
-            demo version. You can choose to download the desktop demo
-            application or visit the web version to get a taste of what we
-            offer.
-          </p>
-          <p className="demo-paragraph">
-            The demo version provides a preview of the full application. While
-            you can explore many features, please note that some functionalities
-            are restricted in the demo. To unlock the complete set of features,
-            we encourage you to try the full version of the application.
-          </p>
-          <p className="demo-paragraph">
-            Click the buttons below to start using the demo. We appreciate your
-            interest in Hospitalline and look forward to assisting you!
-          </p>
-          <span className="demoButtons">
-            <button>Download Demo Application</button>
-            <button>Access Web Demo</button>
-          </span>
-        </div>
-        <div className="image">
-          <img src={ImageThree} />
-        </div>
-      </div>
-    </section>
-  );
-};
-
-export default demoSection;
->>>>>>> 29b66b9429399e29c93bf194e48b4cd8db1fc8ce
